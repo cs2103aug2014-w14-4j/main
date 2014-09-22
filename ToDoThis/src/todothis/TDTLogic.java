0a1a58f658de406ef27e2204fdb2305cff391d7b
Victor <hahavthb@gmail.com>
2014-09-22 16:47:02 +0800

    add code to TDT logic

--- ToDoThis/src/todothis/TDTLogic.java
+++ ToDoThis/src/todothis/TDTLogic.java
@@ -1,5 +1,9 @@
 package todothis;
 
+import java.util.Iterator;
+
+import todothis.ITDTParser.COMMANDTYPE;
+
 
 public class TDTLogic implements ITDTLogic {
 	private TDTStorage storage;
@@ -52,6 +56,9 @@ public class TDTLogic implements ITDTLogic {
 	@Override
 	public String doEdit(Command command) {
 		// TODO Auto-generated method stub
+		String labelName = command.getLabelName();
+		int taskID = command.getTaskID() - 1;
+		
 		return null;
 	}
 
@@ -67,19 +74,81 @@ public class TDTLogic implements ITDTLogic {
 	@Override
 	public void doDisplay(Command command) {
 		// TODO Auto-generated method stub
+		String labelName = command.getLabelName();
+		Iterator<Task> i;
+		if(labelName.equals("")){
+			i = storage.getTaskIterator();
+			while(i.hasNext()){
+				Task temp = i.next();
+				temp.setHide(false);
+			}
+		}else if(storage.getLabelMap().containsKey(labelName)){
+			i = storage.getTaskIterator();
+			while(i.hasNext()){
+				Task temp = i.next();
+				if(temp.getLabelName().equals(labelName)){
+					temp.setHide(false);
+				}else{
+					temp.setHide(true);
+				}
+			}
+		}else{
+			System.out.println("Display command invalid!");
+		}
 		
 	}
 
 	@Override
 	public void doHide(Command command) {
 		// TODO Auto-generated method stub
+		Iterator <Task> i;
+		String labelName = command.getLabelName();
+
+		if(labelName.equals("")){
+			i = storage.getTaskIterator();
+			while(i.hasNext()){
+				i.next().setHide(true);
+			}
+		}else{
+			if(storage.getLabelMap().containsKey(labelName)){
+				i = storage.getLabelMap().get(labelName).iterator();
+				while(i.hasNext()){
+					i.next().setHide(true);
+				}
+			}else{
+				System.out.println("Label name cannot be found!");
+			}
+		}
 		
 	}
-
+	
+	
 	@Override
 	public void doDone(Command command) {
 		// TODO Auto-generated method stub
+		//storage.getUndoStack().push(storage.copyLabelMap());
+		String labelName = command.getLabelName();
+		int taskID = command.getTaskID() - 1;
+		Iterator <Task> i;
+		int counter = 0;
 		
+		if(!storage.getLabelMap().containsKey(labelName)){
+			System.out.println("Label Name cannot be found!");
+		}else if(storage.getLabelMap().get(labelName).size() < taskID ||
+				taskID <=0){
+			System.out.println("TaskID to be marked done cannot be found! OUT OF RANGE!");
+			
+		}else{
+			i = storage.getLabelMap().get(labelName).iterator();
+			while(i.hasNext()){
+				Task temp = i.next();
+				if(counter == taskID){
+					temp.setDone(true);
+					break;
+				}
+				counter++;
+			}
+		}
 	}
 
 }
