20129f9d75a54c68869b2b8ff54f5897fe637989
Jmyn <jermyntanu92@gmail.com>
2014-10-25 01:28:43 +0800

    Allow search for date
    use search @<Date> to indicate date search.

--- ToDoThis/src/todothis/command/SearchCommand.java
+++ ToDoThis/src/todothis/command/SearchCommand.java
@@ -3,6 +3,7 @@ package todothis.command;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import todothis.logic.TDTDateAndTime;
 import todothis.logic.Task;
 import todothis.parser.ITDTParser.COMMANDTYPE;
 import todothis.storage.TDTStorage;
@@ -19,13 +20,44 @@ public class SearchCommand extends Command {
 	
 	@Override
 	public String execute(TDTStorage storage) {
+		
+		Iterator<Task> iter = storage.getTaskIterator();
+		if(searchedWords.equals("")) {
+			searchEveryTask(iter);
+		} else if(searchedWords.trim().charAt(0) != '@') {
+			searchKeyWords(iter);
+		} else {
+			searchByDate(iter);
+		}
+		String feedback = searchedResult.size() + " result(s) found for \"" 
+				+ searchedWords + "\".";
+		return feedback;
+	}
+
+	private void searchByDate(Iterator<Task> iter) {
+		String[] params = TDTDateAndTime.decodeSearchDetails(searchedWords.trim().substring(1).trim()).split(" ");
+		while(iter.hasNext()) {
+			Task task = iter.next();
+			String startDate = task.getDateAndTime().getStartDate();
+			String endDate = task.getDateAndTime().getEndDate();
+			
+			for(int i = 0; i < params.length; i++) {
+				if(params[i].equals(startDate) || params[i].equals(endDate)) {
+					searchedResult.add(task);
+				}
+			}
+		}
+		
+	}
+
+	private void searchKeyWords(Iterator<Task> iter) {
 		boolean found = false;
 		String[] params = searchedWords.replaceAll("[\\W]", " ").split(" ");
-		Iterator<Task> iter = storage.getTaskIterator();
-		
+		//String[] params = searchedWords.split(" ");
 		while(iter.hasNext()) {
 			Task task = iter.next();
 			String[] words = task.getDetails().replaceAll("[\\W]", " ").split(" ");
+			//String[] words = task.getDetails().split(" ");
 			for(int j = 0; j < params.length; j++) {
 				for(int k = 0; k < words.length; k++) {
 					if(words[k].equalsIgnoreCase(params[j])) {
@@ -44,11 +76,14 @@ public class SearchCommand extends Command {
 			} 
 			found = false;
 		}
-		
-		String feedback = searchedResult.size() + " result(s) found for \"" 
-				+ searchedWords + "\".";
-		return feedback;
 	}
+
+	private void searchEveryTask(Iterator<Task> iter) {
+		while(iter.hasNext()) {
+			searchedResult.add(iter.next());
+		}
+	}
+
 
 	public String getSearchedWords() {
 		return searchedWords;
